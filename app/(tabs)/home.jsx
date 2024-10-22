2b516c895e99762ca59a01a1def459e05ae4e2c6
TwickE <118013263+TwickE@users.noreply.github.com>
2024-10-22 15:03:30 +0100

    Finished the create post logic

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
                     <View className="my-6 px-4 space-y-6">
                         <View className="justify-between items-start flex-row mb-6">
                             <View>
-                                <Text className="text-pmedium text-sm text-gray-100">Welcome back,</Text>
+                                <Text className="text-pmedium text-sm text-gray-100">Welcome back</Text>
                                 <Text className="text-2xl font-psemibold text-white">{user?.username}</Text>
                             </View>
                             <View className="mt-1.5">
